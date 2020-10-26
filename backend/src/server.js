7154d06454e9ec991a29d42ca7e023df3079a6bc
h4ppyy <93immm@naver.com>
2020-10-26 22:43:46 +0900

    graphql findAll, findOne example

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,86 @@
-const Koa = require('koa');
-const { ApolloServer, gql } = require('apollo-server-koa');
- 
-// Construct a schema, using GraphQL schema language
+
+import Koa from 'koa';
+import { ApolloServer, gql } from 'apollo-server-koa';
+
+import { Sequelize, DataTypes } from 'sequelize'
+const sequelize = new Sequelize('postgres://admin:password@localhost:5432/japanesehub')
+
+const Account = sequelize.define('account', {
+  // Model attributes are defined here
+  id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+  account: { type: DataTypes.STRING, allowNull: false, unique: 'compositeIndex' },
+  msg: { type: DataTypes.STRING, allowNull: true },
+  password: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: 'compositeIndex' },
+  jlpt: { type: DataTypes.INTEGER, allowNull: false },
+  level: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  deleted: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+  created_at: { type: DataTypes.DATE, allowNull: false, defaultValue: Sequelize.NOW },
+  updated_at: { type: DataTypes.DATE, allowNull: true },
+  deleted_at: { type: DataTypes.DATE, allowNull: true },
+}, {
+  timestamps: false
+});
+
+const tesingDB = async () => {
+  try {
+    await sequelize.authenticate();
+
+    // const jane = Account.build({ 
+    //   account: "user3",
+    //   msg: "good!!!",
+    //   password: "1234",
+    //   email: "user3@naver.com",
+    //   jlpt: 1
+    // });
+    // await jane.save();
+
+    const users = await Account.findAll();
+    console.log('users => ', users)
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+tesingDB();
+
 const typeDefs = gql`
+  scalar Date
+
+  type Account {
+    id: Int!
+    account: String
+    msg: String
+    password: String
+    email: String
+    jlpt: String
+    level: String
+    deleted: String
+    created_at: Date
+    updated_at: Date
+    deleted_at: Date
+  }
+
   type Query {
-    hello: String
+    hello: String!
+    getAccountList: [Account]
+    getAccount(id: Int): Account
   }
 `;
  
-// Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
+    getAccountList: async () => {
+      const result = await Account.findAll({ raw : true })
+      console.log('result => ', result)
+      return result
+    },
+    getAccount: async (root, { id }) => {
+      const result = await Account.findOne({ where: { id: id }, raw : true })
+      console.log('result => ', result)
+      return result
+    }
   },
 };
  
@@ -19,9 +88,7 @@ const server = new ApolloServer({ typeDefs, resolvers });
  
 const app = new Koa();
 server.applyMiddleware({ app });
-// alternatively you can get a composed middleware from the apollo server
-// app.use(server.getMiddleware());
  
 app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`),
 );
