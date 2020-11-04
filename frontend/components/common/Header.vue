d4f160baa18289e18ebde2e7d894b68736de8eb5
h4ppyy <93immm@naver.com>
2020-11-04 21:49:29 +0900

    make menu

--- frontend/components/common/Header.vue
+++ frontend/components/common/Header.vue
@@ -1,15 +1,10 @@
 <template>
   <header>
-    <nav class="navbar container" role="navigation" aria-label="main navigation">
+    <nav class="navbar container is-spaced" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-          <img
-            src="https://bulma.io/images/bulma-logo.png"
-            width="112"
-            height="28"
-          />
+        <a class="navbar-item">
+          Japanese Hub
         </a>
-
         <a
           role="button"
           class="navbar-burger burger"
@@ -22,38 +17,44 @@
           <span aria-hidden="true"></span>
         </a>
       </div>
-
       <div id="navbarBasicExample" class="navbar-menu">
         <div class="navbar-start">
-          <div class="navbar-item">
-          <input class="input" type="text" placeholder="e.g Alex Smith">
-          </div>
+          <!-- not use -->
         </div>
-
         <div class="navbar-end">
-          <a class="navbar-item"> Home </a>
 
-          <a class="navbar-item"> Documentation </a>
+          <a class="navbar-item">Repository</a>
+
+          <a class="navbar-item">Problem</a>
 
           <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link"> More </a>
-
+            <a class="navbar-link">Ranking</a>
             <div class="navbar-dropdown">
-              <a class="navbar-item"> About </a>
-              <a class="navbar-item"> Jobs </a>
-              <a class="navbar-item"> Contact </a>
-              <hr class="navbar-divider" />
-              <a class="navbar-item"> Report an issue </a>
+              <a class="navbar-item">Problem Solving Base Ranking</a>
+              <a class="navbar-item">Activity Base Ranking</a>
             </div>
           </div>
+
+          <!-- status: not login -->
           <div class="navbar-item">
             <div class="buttons">
               <a class="button is-primary">
                 <strong>Sign up</strong>
               </a>
-              <a class="button is-light"> Log in </a>
+              <a class="button is-light">Sign in </a>
             </div>
           </div>
+
+          <!-- status: login -->
+          <!-- <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">h4ppyy</a>
+            <div class="navbar-dropdown">
+              <a class="navbar-item">My Profile</a>
+              <hr class="navbar-divider">
+              <a class="navbar-item">Log out</a>
+            </div>
+          </div> -->
+
         </div>
       </div>
     </nav>
