cffc3d77b631d5a908ba626ca102e5eb31a9c6d4
h4ppyy <93immm@naver.com>
2020-11-04 17:26:39 +0900

    adding Header and Footer

--- frontend/components/common/Header.vue
+++ frontend/components/common/Header.vue
@@ -1,5 +1,61 @@
 <template>
-  <div>
-    Header
-  </div>
+  <header>
+    <nav class="navbar container" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+          />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <div class="navbar-item">
+          <input class="input" type="text" placeholder="e.g Alex Smith">
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <a class="navbar-item"> Home </a>
+
+          <a class="navbar-item"> Documentation </a>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link"> More </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item"> About </a>
+              <a class="navbar-item"> Jobs </a>
+              <a class="navbar-item"> Contact </a>
+              <hr class="navbar-divider" />
+              <a class="navbar-item"> Report an issue </a>
+            </div>
+          </div>
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light"> Log in </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
     <div class="test">www</div>
-    <button class="button is-primary">Primary</button>
 
-    <span class="icon has-text-info">
-      <i class="far fa-angry"></i>
-    </span>
-    <span class="icon has-text-success">
-      <i class="fas fa-bell"></i>
-    </span>
-    <span class="icon has-text-warning">
-      <i class="fas fa-exclamation-triangle"></i>
-    </span>
-    <span class="icon has-text-danger">
-      <i class="fas fa-ban"></i>
-    </span>
+    <div>
+      <button class="button is-primary">Primary</button>
+    </div>
+
+    <div>
+      <span class="icon has-text-info">
+        <i class="far fa-angry"></i>
+      </span>
+      <span class="icon has-text-success">
+        <i class="fas fa-bell"></i>
+      </span>
+      <span class="icon has-text-warning">
+        <i class="fas fa-exclamation-triangle"></i>
+      </span>
+      <span class="icon has-text-danger">
+        <i class="fas fa-ban"></i>
+      </span>
+    </div>
+    
   </div>
 </template>
 
